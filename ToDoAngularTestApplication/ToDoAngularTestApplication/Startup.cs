16ac93eb3074577f8477b813164ded862c6531a6
Emir Osmanoski <emir.osmanoski@gmail.com>
2014-02-21 14:00:19 +0100

    Initial project commit.

---
+++ ToDoAngularTestApplication/ToDoAngularTestApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoAngularTestApplication.Startup))]
+namespace ToDoAngularTestApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
